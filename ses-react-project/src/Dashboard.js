8f1a60a144ef6d3863d5b78d3bcb7375d622a1e0
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-07-23 08:17:31 +0000

    Update Dashboard.js

--- ses-react-project/src/Dashboard.js
+++ ses-react-project/src/Dashboard.js
@@ -13,8 +13,7 @@ const images = [
     require('./5.jpg'),
 ];
 
-
-function Dashboard() {
+const Dashboard = () => {
     return (
         <div className="dashboard-container">
             <div className="sidebar">
@@ -49,10 +48,31 @@ function Dashboard() {
                         </Link>
                     </li>
                 </ul>
+                {/* Additional Quick Links */}
+                <div className="sidebar-quick-links">
+                    <h2>Quick Links</h2>
+                    <ul>
+                        <li>
+                            <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjktrSPsZ2AAxVMh1wKHZ_LDu0QFnoECBQQAQ&url=https%3A%2F%2Fienabler.ug.edu.gh%2Fpls%2Fprodi41%2Fw99pkg.mi_login&usg=AOvVaw2ulwdt-4iU9i8kDQqxlnmT&opi=89978449" target="_blank" rel="noopener noreferrer">MISWEB</a>
+                        </li>
+                        <li>
+                            <a href="https://sakai.ug.edu.gh/" target="_blank" rel="noopener noreferrer">SAKAI</a>
+                        </li>
+                        <li>
+                            <a href="https://sts.ug.edu.gh/" target="_blank" rel="noopener noreferrer">STS</a>
+                        </li>
+                        <li>
+                            <a href="https://www.ug.edu.gh/" target="_blank" rel="noopener noreferrer">UG Website</a>
+                        </li>
+                        <li>
+                            <a href="https://balme.ug.edu.gh/" target="_blank" rel="noopener noreferrer">UG Library</a>
+                        </li>
+                    </ul>
+                </div>
                 <div className="sidebar-footer"></div>
             </div>
             <div className="main-content">
-                <h1 className="main-heading">Welcome to SES Dashboard</h1>
+                <h1 className="main-heading">Welcome to SES Dashboard, Evans Acheampong</h1>
                 <p className="main-text">
                     "Science can amuse and fascinate us all, but it is engineering that
                     changes the world." - Isaac Asimov
@@ -73,7 +93,7 @@ function Dashboard() {
                     <Carousel showThumbs={false} infiniteLoop autoPlay>
                         {images.map((imageUrl, index) => (
                             <div key={index}>
-                                <img className="slideshow-image" src={imageUrl} alt={`Slide ${index}`} />
+                                <img className="slideshow-image" src={imageUrl.default} alt={`Slide ${index}`} />
                             </div>
                         ))}
                     </Carousel>
@@ -84,3 +104,4 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
